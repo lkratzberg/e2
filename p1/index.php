902f8d6690b01b17761c0c9ec4a603f2b65984a9
Lyndsey Kratzberg <lkratzberg@yahoo.com>
2019-09-17 16:56:06 -0500

    Created Project 1 files

---
+++ p1/index.php
@@ -0,0 +1,7 @@
+<h1>Lyndsey Kratzberg</h1>
+<img alt='Pic of Lyndsey Kratzberg' src='/images/picofme.jpg'>
+<p>Hi! My name is Lyndsey Kratzberg and I am pursuing a Graduate Certificate in Programming. I am currently a 
+high school computer science teacher. The classes I teach are dual credit at the local community college, 
+which requires me to further my education with graduate credit hours. My background is in Accounting, not 
+programming, and I spent 11 years in the corporate business world before transitioning to teaching a couple of 
+years ago. I absolutely love the career change and love learning what I can do with programming.</p>
