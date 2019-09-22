e52a2d7b27fd4acce60c2a7304a3796e57f438ef
Lyndsey Kratzberg <lkratzberg@yahoo.com>
2019-09-22 16:07:09 -0500

    Needed to add words to make sure production server working

---
+++ p2/index.php
@@ -0,0 +1,18 @@
+<?php require index-controller.php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>My Web Site</title>
+    <meta charset='utf-8'>
+
+    <!-- page config goes here in the head -->
+
+</head>
+<body>
+
+    <p> Project 2 goes here </p>
+
+</body>
+</html>
