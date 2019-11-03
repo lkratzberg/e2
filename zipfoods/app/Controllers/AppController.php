3eebe3ed65859d53b2a01147feacbd67ee9034ef
Lyndsey Kratzberg <lkratzberg@yahoo.com>
2019-11-03 14:42:55 -0600

    Week9 Practice

--- zipfoods/app/Controllers/AppController.php
+++ zipfoods/app/Controllers/AppController.php
@@ -9,9 +9,14 @@ class AppController extends Controller
     public function index()
     {
         $welcomes = ['Welcome', 'Aloha!', 'Welkom', 'Bienvenidos', 'Bienvenu', 'Welkomma'];
-        
+
         return $this->app->view('index', [
             'welcome' => $welcomes[array_rand($welcomes)]
         ]);
+    }
+
+    public function about()
+    {
+      return 'zipfoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.';
     }
 }
